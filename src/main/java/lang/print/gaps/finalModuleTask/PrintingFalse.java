2ff258f0c79b698145a1f398c52d83983cb937a1
PavelDenisov <pavel.den111@gmail.com>
2023-02-14 15:19:09 +0200

    done

--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -4,6 +4,18 @@ public class PrintingFalse {
     public static void main(String[] args) {
         int first = 9;
         int second = -17;
+        boolean result = first > 10 && second < -10;
+        System.out.println(result);
         //write code here
     }
 }
+//1) Print false.
+//        You will be given 2 variables and your task will be to print false using those vars:
+//
+//public class PrintingFalse {
+//    public static void main(String[] args) {
+//        int first = 9;
+//        int second = -17;
+//        //write code here
+//    }
+//}
